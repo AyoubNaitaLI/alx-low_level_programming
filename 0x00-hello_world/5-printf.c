64a4d68d42e97cee91aaca0975b17bbc3aa8b66d
AyoubNaitaLI <naitaliayoub4@gmail.com>
2023-07-13 15:58:39 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
+/**
+ * a program print using printf
+ * a
+ * */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
