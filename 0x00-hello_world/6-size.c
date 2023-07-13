4690eb6c42ffbfa8f13d1df83c9d657aca179eac
AyoubNaitaLI <naitaliayoub4@gmail.com>
2023-07-13 15:06:48 -0700

    hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (void)
+{
+	printf("size of char \n",sizeof(char));
+	printf("size of int \n",sizeof(int));
+	printf("size of a long int \n",sizeof (long int));
+	printf("size of a long long int \n",sizeof (long long int));
+	printf("size of a float \n",sizeof (float));
+	return(0);
+}
