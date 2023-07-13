e262e00d1350b9cbb580d00be642472ce5ce85ac
AyoubNaitaLI <naitaliayoub4@gmail.com>
2023-07-13 16:08:10 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main - a prigram that print using printf
- * Return : 0 (success)
- * */
+* main - a prigram that print using printf
+* Return :Always 0 (success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
